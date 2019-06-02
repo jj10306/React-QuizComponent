82ad8d0e3f8880827976863a971952df6a8571d7
Jakob Johnson <johnsonjakob99@gmail.com>
2019-06-02 16:00:39 -0700

    Quiz component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
+import Quiz from './Quiz.js'
+
+let quizData = require('./quiz_data.json');
+console.log(quizData["quiz_questions"][0]["instruction_text"]);
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
